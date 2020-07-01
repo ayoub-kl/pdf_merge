4883d02bdd2f7fba1481c12525e451f6ec05818e
ayoub-kl <46133199+ayoub-kl@users.noreply.github.com>
2020-07-01 14:06:58 +0100

    poc

---
+++ merge_pdf_compo_16.4dbase/Resources/pdf_merger_script.py
@@ -0,0 +1,14 @@
+﻿
+from PyPDF2 import PdfFileMerger
+
+pdfs = [pdf1_path,pdf2_path]
+
+merger = PdfFileMerger()
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write(pdf_res_path)
+merger.close()
+
+
